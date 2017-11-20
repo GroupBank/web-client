8951d10c0209b96cda13f36e77af4d367e0836ca
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2017-11-20 23:17:47 +0000

    Add BitcoinJS library and an example of address generation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import * as settings from './settings';
+import * as bitcoin from 'bitcoinjs-lib';
 
 console.log("Hello from JavaScript!");
+console.log("Here's your address: " + bitcoin.ECPair.makeRandom().toWIF())
 
 fetch(settings.url, {
     method: "GET",
