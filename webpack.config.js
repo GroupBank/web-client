fb2bff6e1b24d9bc5775e9916382421e39f9fae7
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2018-02-25 18:49:57 +0000

    Migrate to webpack 4.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: './dist/bundle.js'
+    filename: './bundle.js'
   },
   resolve: {
     extensions: ['.js'],
